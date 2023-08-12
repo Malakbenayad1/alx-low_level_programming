7e312d3bb32b7e3fdfb239a17ca9005bc3365caa
Malakbenayad1 <mm8584856@gmail.com>
2023-08-11 20:40:47 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,36 @@
-#include <stdio.h>
-/**
- * main - Entry point
- * 
- * Return: always 0
+/*
+ * File: 100-print_comb3.c
+ * Auth: Brennan D Baraban
  */
-int maim(void)
-{
-	int ones = '0';
-	int tens = '0';
 
-	for (tens = '0'; tens <= '9'; ones++)
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-			if (!((ones == tens) || (tens > ones)))
-			{
-				putchar(tens);
-				putchar(ones);
-				if (!(ones == '9' && tens == '8'))
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
