6740a4f65779b121d7e6d71330838e146d15a2af
Malakbenayad1 <mm8584856@gmail.com>
2023-08-11 18:37:58 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: "print alphabets lpwercase and uppercase"
+ * Description: 'print alphabets lowercase and uppercase'
  * Return: always 0
  */
 int main(void)
 {
 	int n = 97;
-	int n = 65;
+	int m = 65;
 
 	while (n <= 122)
 	{
@@ -16,7 +16,7 @@ int main(void)
 	}
 	while (m <= 90)
 	{
-		putchsr(m);
+		putchar(m);
 		m++;
 	}
 	putchar('\n');
