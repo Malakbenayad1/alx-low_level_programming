f5921a8d766f0d63f4f79568d8ab485d4040f43f
Malakbenayad1 <mm8584856@gmail.com>
2023-09-07 13:46:45 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,8 +21,14 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		d = malloc(new_size);
 		return (d);
 	}
+	else if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
 	else if (new_size == old_size)
 		return (ptr);
+
 	d = malloc(new_size);
 	if (d == NULL)
 		return (NULL);
