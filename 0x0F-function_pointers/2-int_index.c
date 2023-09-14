eab870087b63d5edf93c90139432e195dcc2d7a5
Malakbenayad1 <mm8584856@gmail.com>
2023-09-13 18:51:38 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
 
 /**
  * int_index - return index place if comparison = true, else -1
- * array: array
+ * @array: array
  * @size: size of elements in array
  * @cmp: pointer to func of one of the 3 in main
  * Return: 0
