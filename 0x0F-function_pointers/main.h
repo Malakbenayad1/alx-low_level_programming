9eefe9d9a2acdb4ffb6a53783ab1fde40647f10b
Malakbenayad1 <mm8584856@gmail.com>
2023-09-13 18:01:25 -0700

    task 0

--- 0x0F-function_pointers/main.h
+++ 0x0F-function_pointers/main.h
@@ -1,7 +1,7 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
