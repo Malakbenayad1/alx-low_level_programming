8e319af947a44e7a047bdc01eda34c86b803d807
Malakbenayad1 <benmaalak1@gmail.com>
2024-02-09 14:04:10 -0800

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,21 +1,34 @@
 #include "main.h"
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the unsigned int form of b
- */
-
-unsigned int binary_to_uint(const char *b);
+  * binary_to_uint - converts a binary number to an unsigned int
+  * @b: binary number
+  * Return: the converted number or 0 if it fails
+  */
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
+	unsigned int uint, place;
+	int n;
+
 	if (!b)
 		return (0);
-	while (*b)
+
+	for (n = 0; b[n]; n++)
+		;
+	n--;
+	for (place = 1, uint = 0; n >= 0; n--)
 	{
-		if (*b != "0" && *b != "1")
-			return (0);
-		num = num * 2 + (*b++ - "0");
+		if (b[n] == '0')
+		{
+			place *= 2;
+			continue;
+		}
+		else if (b[n] == '1')
+		{
+			uint += place;
+			place *= 2;
+			continue;
+		}
+		return (0);
 	}
-	return (num);
+	return (uint);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-#include <stdio.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
