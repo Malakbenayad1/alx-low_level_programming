ea26f9a512eb659b158dabcfa9cabb4308eb6637
Malakbenayad1 <benmaalak1@gmail.com>
2024-02-09 15:28:58 -0800

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * git_bit - returns the value of a bit at a given index
+ * index: the index
+ * @n: unsigned long int to search
+ * Return: the value of the bit at index or -1 if error
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 54)
+		return (-1);
+	return ((n >> index) & 1);
+}
