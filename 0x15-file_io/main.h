0d05e4f84faeb80289e872f3bfc65a39617f8a3c
Malakbenayad1 <benmaalak1@gmail.com>
2024-02-12 09:26:08 -0800

    task 0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,28 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN.H
+#define MAIN.H
 
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <elf.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-/* Function prototypes */
-void error_exit(int code, char *message);
-void print_magic(unsigned char *magic);
-void print_class(unsigned char class);
-void print_data(unsigned char data);
-void print_version(unsigned char version);
-void print_osabi(unsigned char osabi);
-void print_type(unsigned int type);
-void print_entry(unsigned long int entry);
-void read_elf_header(char *filename);
-void close_file(int FD);
-int create_file(const char *filename, char *text_content);
-int _strlen(char *str);
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#define BUF_SIZE 1024
 
-#endif /* MAIN_H */
+#endif
